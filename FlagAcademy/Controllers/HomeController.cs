3a254bf459a4ecdd2298f7a3dbe35f02e905beac
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-03 11:16:57 -0700

    Generate random wrong countries with a correct answer in a random order

--- FlagAcademy/Controllers/HomeController.cs
+++ FlagAcademy/Controllers/HomeController.cs
@@ -26,10 +26,57 @@ namespace FlagAcademy.Controllers
         public IActionResult Play()
         {
             Random rnd = new Random();
-            int rndnum = rnd.Next(1, 6);
-            var country = _context.Countries.Where(c=>c.Id == rndnum).First();
+            int randomFlag = rnd.Next(1, 6);
+
+            var country = _context.Countries.Where(c=>c.Id == randomFlag).First();
+            var countries = _context.Countries.ToList();
+
+            ViewData["countries"] = GenerateWrongAnswers(country);
+
             return View(country);
         }
+
+
+        public List<Country> GenerateWrongAnswers(Country correctAnswer)
+        {
+            List<Country> wrongAnswers = new List<Country>();
+            Random rnd = new Random();
+            
+            while (wrongAnswers.Count < 4)
+            {
+                int num = rnd.Next(1, 6);
+                var country = _context.Countries.Where(c => c.Id == num).First();
+                //makes sure your not adding duplicates of the correct guess of two of the same wrong answers.
+                if(country != correctAnswer && !wrongAnswers.Contains(country))
+                {
+                    wrongAnswers.Add(country);
+                }
+            }
+            var shuffledCountries = ShuffleGuesses(wrongAnswers, correctAnswer);
+            return shuffledCountries;
+        }
+
+        public List<Country> ShuffleGuesses(List<Country>countries, Country correctAnswer)
+        {
+            List<Country> shuffledCountries = new List<Country>();
+            Random rnd = new Random();
+            int position = rnd.Next(0, 4);
+
+            for (int i = 0; i < countries.Count; i++)
+            {
+                if(i == position)
+                {
+                    shuffledCountries.Add(correctAnswer);
+                }
+                else
+                {
+                    shuffledCountries.Add(countries[i]);
+                }
+            }
+
+            return shuffledCountries;
+        }
+
 
 
 
